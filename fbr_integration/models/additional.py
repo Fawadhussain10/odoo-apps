62be90a80a97417d697dde8c8eab608752af597f
Fawad Hussain <fawadh817@gmail.com>
2026-05-13 17:34:59 +0500

    updates

--- fbr_integration/models/additional.py
+++ fbr_integration/models/additional.py
@@ -17,12 +17,26 @@ class ResPartner(models.Model):
 class ProductTempExt(models.Model):
     _inherit = 'product.template'
 
-    pct_code = fields.Char("PCT/HS Code", required=True)
-    sale_type = fields.Char(string='Sale Type', required=True)
-    sro_schedule = fields.Char(string='SRO Schedule No')
-    sro_item = fields.Char(string='SRO Item No')
-    fed_duty = fields.Many2one('account.tax', string='FED Duty')
-    further_tax = fields.Many2one('account.tax', string='Further Tax')
-    extra_tax = fields.Many2one('account.tax', string='Extra Tax')
-    other_tax = fields.Char(string='Other Type Tax (e.g. Exempt)')
-    withholding_tax = fields.Many2one('account.tax', string='Withholding Tax')
+    pct_code = fields.Char("PCT/HS Code", required=True, store=True)
+    sale_type = fields.Char(string='Sale Type', required=True, store=True)
+    sro_schedule = fields.Char(string='SRO Schedule No', store=True)
+    sro_item = fields.Char(string='SRO Item No', store=True)
+    fed_duty = fields.Many2one('account.tax', string='FED Duty', store=True)
+    further_tax = fields.Many2one('account.tax', string='Further Tax', store=True)
+    extra_tax = fields.Many2one('account.tax', string='Extra Tax', store=True)
+    other_tax = fields.Char(string='Other Type Tax (e.g. Exempt)', store=True)
+    withholding_tax = fields.Many2one('account.tax', string='Withholding Tax', store=True)
+
+
+class ProductProductExt(models.Model):
+    _inherit = 'product.product'
+
+    pct_code = fields.Char("PCT/HS Code", required=True, store=True)
+    sale_type = fields.Char(string='Sale Type', required=True, store=True)
+    sro_schedule = fields.Char(string='SRO Schedule No', store=True)
+    sro_item = fields.Char(string='SRO Item No', store=True)
+    fed_duty = fields.Many2one('account.tax', string='FED Duty', store=True)
+    further_tax = fields.Many2one('account.tax', string='Further Tax', store=True)
+    extra_tax = fields.Many2one('account.tax', string='Extra Tax', store=True)
+    other_tax = fields.Char(string='Other Type Tax (e.g. Exempt)', store=True)
+    withholding_tax = fields.Many2one('account.tax', string='Withholding Tax', store=True)
