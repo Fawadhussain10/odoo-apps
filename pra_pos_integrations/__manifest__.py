da0168b667ac41b5db866915a172617191c1ed46
Fawad Hussain <fawadh817@gmail.com>
2026-09-24 13:02:06 +0500

    updates

--- pra_pos_integrations/__manifest__.py
+++ pra_pos_integrations/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "PRA POS Integration (Punjab Revenue Authority)",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "summary": "Report POS sales to the Punjab Revenue Authority (PRA) e-IMS in real time",
     "description": """
 Punjab Revenue Authority - POS / e-IMS Integration
